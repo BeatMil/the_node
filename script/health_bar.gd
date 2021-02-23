c62cf7e1849350e2a894586b9b0ca5f5288c60fc
beatmil <anutorn@gmail.com>
2021-02-23 16:37:03 +0700

    health_bar.tscn now works properly!!  I did it!  Three colors (Green, Orange and Red)  The problem was that every instances use the same styleboxflat,  so when it changes all of them also changed  Changes to be committed:         modified:   media/stylebox/green_stylebox.tres         new file:   media/stylebox/orange_stylebox.tres 	new file:   media/stylebox/red_stylebox.tres 	modified:   prefab/enemy.tscn 	modified:   prefab/health_bar.tscn 	modified:   prefab/home.tscn 	modified:   script/health_bar.gd 	deleted:    test_area/beat_test.gd 	deleted:    test_area/beat_test.tscn

--- script/health_bar.gd
+++ script/health_bar.gd
@@ -1,33 +1,24 @@
 extends ProgressBar
 
-# set of colors
-const BLUE = Color(0.03, 0.25, 0.76)
-const GREEN = Color(0.25, 0.80, 0.20)
-const ORANGE = Color(0.81, 0.32, 0.09)
-const RED = Color(0.87, 0.11, 0.11)
 
 const GREEN_BOX = preload("res://media/stylebox/green_stylebox.tres")
+const RED_BOX = preload("res://media/stylebox/red_stylebox.tres")
+const ORANGE_BOX = preload("res://media/stylebox/orange_stylebox.tres")
 
-# call once and done
-#var style_box = $".".get("custom_styles/fg")
 
 
 func _ready():
-	var green_box = StyleBoxFlat
-#	green_box.bg_color = GREEN
-	print(green_box.bg_color)
 	health_color_adjuster()
-
-
+	
 func _on_health_bar_value_changed(_value):
 	health_color_adjuster()
 
 
 func health_color_adjuster():
 	var percent = value/max_value
-	if percent >= 0.6:
-		self.get("custom_styles/fg") = GREEN_BOX
+	if percent >= 0.7:
+		set("custom_styles/fg",GREEN_BOX)
 	elif percent >= 0.4:
-		self.get("custom_styles/fg").bg_color = ORANGE
+		set("custom_styles/fg",ORANGE_BOX)
 	else:
-		self.get("custom_styles/fg").bg_color = RED
+		set("custom_styles/fg",RED_BOX)
