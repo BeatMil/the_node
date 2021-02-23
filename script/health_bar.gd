c700404eac57eedae8957ae6b51f38ed4050baf5
BeatMil <anutorn@gmail.com>
2021-02-23 15:18:06 +0700

    Still in progress of making health_bar.tscn that changes color  Bug: All of the health bar has the same color regarless of health  Changes to be committed: 	new file:   media/stylebox/green_stylebox.tres 	modified:   prefab/enemy.tscn 	modified:   prefab/health_bar.tscn 	modified:   prefab/home.tscn 	modified:   project.godot 	modified:   scene/beat.tscn 	modified:   script/health_bar.gd

--- script/health_bar.gd
+++ script/health_bar.gd
@@ -1,16 +1,33 @@
 extends ProgressBar
 
+# set of colors
+const BLUE = Color(0.03, 0.25, 0.76)
+const GREEN = Color(0.25, 0.80, 0.20)
+const ORANGE = Color(0.81, 0.32, 0.09)
+const RED = Color(0.87, 0.11, 0.11)
+
+const GREEN_BOX = preload("res://media/stylebox/green_stylebox.tres")
+
+# call once and done
+#var style_box = $".".get("custom_styles/fg")
+
 
 func _ready():
-	var style_box = $".".get("custom_styles/fg")
-	style_box.bg_color = Color(0.03, 0.25, 0.76)
+	var green_box = StyleBoxFlat
+#	green_box.bg_color = GREEN
+	print(green_box.bg_color)
+	health_color_adjuster()
 
 
-func _on_health_bar_value_changed(value):
-	pass # Replace with function body.
+func _on_health_bar_value_changed(_value):
+	health_color_adjuster()
 
 
 func health_color_adjuster():
-	if value/max_value >= 0.6:
-		pass
-	pass
+	var percent = value/max_value
+	if percent >= 0.6:
+		self.get("custom_styles/fg") = GREEN_BOX
+	elif percent >= 0.4:
+		self.get("custom_styles/fg").bg_color = ORANGE
+	else:
+		self.get("custom_styles/fg").bg_color = RED
