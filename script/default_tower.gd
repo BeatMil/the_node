4457816f5f5e4c1469a0350d23ffdf8b2a005578
BeatMil <anutorn@gmail.com>
2021-02-24 12:46:50 +0700

    I recreated the tower.  I deteled default_tower.tscn and created template_tower.tscn instead.  This way, we can create new tower with template_tower!  with same script and different area2D without having to connect signal  cause I connected it with code!  I'm such a genius >///<  Changes to be committed: 	deleted:    prefab/default_tower.tscn 	new file:   prefab/tower/horizontal_tower.tscn 	new file:   prefab/tower/small_tower.tscn 	new file:   prefab/tower/template_tower.tscn 	modified:   scene/beat.tscn 	modified:   script/default_tower.gd

--- script/default_tower.gd
+++ script/default_tower.gd
@@ -4,16 +4,20 @@ export var damage = 1
 export var speed = 0.3
 var enemy_in_range = []
 
+
 func _ready():
 	# try connecting with code
-	$speed_timer.connect("timeout",self,"timer_timeout") 
+	# it worked! and it is super good!
+	$speed_timer.connect("timeout",self,"timer_timeout")
+	$Area2D.connect("area_entered",self,"area_enter_tower")
+	$Area2D.connect("area_exited",self,"area_exit_tower")
 
 	# set fire-rate of this tower
 	$speed_timer.set_wait_time(speed)
 	print("default_tower.gd")
 
 
-func _on_Area2D_area_entered(area):
+func area_enter_tower(area):
 	# pushes area(enemy) into array
 	# always fire to the first index of the array
 	if area.is_in_group("enemy"):
@@ -24,10 +28,8 @@ func _on_Area2D_area_entered(area):
 			enemy_in_range.push_back(area)
 
 
-
-
-func _on_Area2D_area_exited(area):
-	# area(enemy) leaves the area 
+func area_exit_tower(area):
+	# area(enemy) leaves the area
 	# pop them out of the array
 	enemy_in_range.pop_front()
 	if enemy_in_range.empty():
