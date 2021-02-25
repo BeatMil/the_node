2c62685dad31ae94ca2e6a3578b20fe0c5bed4db
ThiwakonPB <50686321+ThiwakonPB@users.noreply.github.com>
2021-02-25 14:16:52 +0700

    added tilemap to create restricted build areas
    edit some redundant code? and added tilemap

--- script/beat.gd
+++ script/beat.gd
@@ -3,6 +3,7 @@ extends Node2D
 const ENEMY_PRE = preload("res://prefab/enemies/enemy.tscn")
 const SMALL_TOWER_PRE = preload("res://prefab/tower/small_tower.tscn")
 var current_grid_pos = Vector2.ZERO
+var can_spawn = true
 onready var auto_beat = get_node("/root/AutoBeat")
 
 
@@ -28,20 +29,40 @@ func spawn_enemy():
 	enemy.way = "../way1/"
 	self.add_child(enemy)
 
+#copy and paste boi. this code just checks if the mouse is colliding with anything
+#useiing to check if it's colliding with a tile that has a collision. We's use this 
+#as a way to create a restricted area where players can't build.
+func _physics_process(delta):
+	# get the Physics2DDirectSpaceState object
+	var space = get_world_2d().direct_space_state
+	# Get the mouse's position
+	var mousePos = get_global_mouse_position()
+	# Check if there is a collision at the mouse position
+	if space.intersect_point(mousePos, 1 ):  
+		print("hit something")
+		can_spawn = false
+	else:
+		print("no hit")
+		can_spawn = true
+
 
 func spawn_small_tower(position):
 	# check if it overlaps other tower before spawning one
 	# by searching for each node in tower group
-	var can_spawn = true
+	#moved the can_spawn out of this function so i can use it in another
 	var is_enough_money = false
-	var towers = get_tree().get_nodes_in_group("tower")
-	for node in towers:
-		if node.get_global_position() == position:
-			can_spawn = false
-	var buttons = get_tree().get_nodes_in_group("button")
-	for node in buttons:
-		if node.get_global_position() == position:
-			can_spawn = false
+	
+	#code below is redundant?
+	
+#	var towers = get_tree().get_nodes_in_group("tower")
+#	for node in towers:
+#		if node.get_global_position() == position:
+#			can_spawn = false
+#	var buttons = get_tree().get_nodes_in_group("button")
+#	for node in buttons:
+#		if node.get_global_position() == position:
+#			can_spawn = false
+
 	# checks money
 	if auto_beat.money - auto_beat.small_tower_price < 0:
 		print("not enough money")
