8e914c739b42647bc0f0c1a11400311639049a46
beatmil <anutorn@gmail.com>
2021-03-27 17:36:49 +0700

    added healthbar and intro animation

--- script/stage3.gd
+++ script/stage3.gd
@@ -3,3 +3,8 @@ extends Node2D
 func _ready():
 	$"enemy/ani_player".play("intro")
 	$"spider/ani_player".play("intro")
+
+
+func _on_ani_player_animation_finished(anim_name):
+	if anim_name == "intro":
+		$"boss_health_bar/ani_player".play("intro")
