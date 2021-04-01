ff556cb902f64203ed768d9ed53dd2830a7befe3
beatmil <anutorn@gmail.com>
2021-04-01 09:11:13 +0700

    boss_cursor wont perfectly follow mouse...

--- script/boss_cursor.gd
+++ script/boss_cursor.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 
 func _ready():
-	pass
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 
-func _physics_process():
-	pass
+func _physics_process(_delta):
+	self.position = get_global_mouse_position()
--- script/stage3.gd
+++ script/stage3.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 # preload the attacks
 const ATTACK1 = preload("res://prefab/boss/attack1.tscn")
+const MOUSE = preload("res://prefab/boss/boss_cursor.tscn")
 
 # play intro animation
 func _ready():
@@ -18,9 +19,17 @@ func _on_ani_player_animation_finished(anim_name):
 
 func _on_Timer_timeout():
 	spawn_attack1()
+	spawn_mouse()
 
 
 func spawn_attack1():
 	var attack1 = ATTACK1.instance()
 	attack1.position = $attack1_pos.position
 	self.add_child(attack1)
+
+
+func spawn_mouse():
+	var mouse = MOUSE.instance()
+	mouse.position = get_global_mouse_position()
+	self.add_child(mouse)
+	pass
